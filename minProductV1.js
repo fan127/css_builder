7ef37e5484756d77df7209174c71cecbca0eccea
fan127 <fan127456@gmail.com>
2022-11-29 13:45:31 +0700

    Update minProductV1.js

--- minProductV1.js
+++ minProductV1.js
@@ -2,25 +2,25 @@
     m.minV1Product = function (c, q) {
         var n = {
             click2cart: "0"
-        }, 
-        context = this,
-        pid = '',
-        section = (this.settings = {}, m(c));
-        this.init = function() {
+        },
+            context = this,
+            pid = '',
+            section = (this.settings = {}, m(c));
+        this.init = function () {
             this.settings = m.extend({}, n, q);
-            console.log(section,'section');
-            console.log(this.settings,'setting');
+            console.log(section, 'section');
+            console.log(this.settings, 'setting');
             return {
 
             }
         }
         this.init();
     }
-    m.fn.minV1Product = function(e) {
-        return this.each(function() {
+    m.fn.minV1Product = function (e) {
+        return this.each(function () {
             var t;
-            null == m(this).data("minv1product") && (t = new m.minV1Product(this,e),
-            m(this).data("minv1product", t))
+            null == m(this).data("minv1product") && (t = new m.minV1Product(this, e),
+                m(this).data("minv1product", t))
         })
     }
-}(jQuery));
+}(window.MinQuery || jQuery));
