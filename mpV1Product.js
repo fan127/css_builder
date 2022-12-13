0bb8a9c88455c76ee36c69768af216df7711357e
fan127 <fan127456@gmail.com>
2022-12-13 10:54:31 +0700

    Update mpV1Product.js

--- mpV1Product.js
+++ mpV1Product.js
@@ -1265,6 +1265,9 @@ function replaceImageToSize(t, e) {
                 section.append('<div class="sg_image-loading-wrap"><div class="sg_image-loading"><div></div><div></div><div></div><div></div></div></div>'),
                     s = section.find(".sg_image-loading-wrap");
                 var t = context.findParentProduct();
+                (t = t.data("mpv1product").getVariant(),
+                        context.initWithVariant(t, !0),
+                        context.listenVariantChange())
                 return context.setFirstVideo(),
                     null != t.data("mpv1product") && (t = t.data("mpv1product").getVariant(),
                         context.initWithVariant(t, !0),
@@ -1563,7 +1566,7 @@ function replaceImageToSize(t, e) {
                         e
                 }
                 ,
-                this.init()
+                this.init();
         }
             ,
             o.fn.minV1ProductImage = function (e) {
